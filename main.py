47bd7f887cf0e0544d8fcf008a557a6e7c6d5f93
Ian <ianflippen@hotmail.com>
2025-08-09 15:30:43 +0200

    Cleanup

--- main.py
+++ main.py
@@ -2,15 +2,11 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-import constants
 from constants import *
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     pygame.init()
-    #print("Starting Asteroids!")
-    #print("Screen width:", constants.SCREEN_WIDTH,)
-    #print("Screen height:", constants.SCREEN_HEIGHT,)
     while True != False:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
